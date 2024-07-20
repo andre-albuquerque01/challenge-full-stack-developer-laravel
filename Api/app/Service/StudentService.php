7b84a66cb012f655eb7d31e91eeeeae472b469a6
andre <andrealbuquerque2001@gmail.com>
2024-07-20 12:43:56 -0300

    feat: create service

---
+++ Api/app/Service/StudentService.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Service;
+
+use App\Exceptions\GeneralException;
+use App\Http\Resources\GeneralResource;
+use App\Http\Resources\StudentResource;
+use App\Models\Student;
+
+class StudentService
+{
+    public function index()
+    {
+        try {
+            $students = Student::all();
+            return StudentResource::collection($students);
+        } catch (\Exception $e) {
+            throw new GeneralException('', $e->getCode(), $e);
+        }
+    }
+
+    public function store(array $data)
+    {
+        try {
+            $cpf = preg_replace('/[^0-9]/', '', $data["cpf"]);
+            $data["cpf"] = substr($cpf, 0, 3) . '.' . substr($cpf, 3, 3) . '.' . substr($cpf, 6, 3) . '-' . substr($cpf, 9, 2);
+            Student::create($data);
+            return new GeneralResource(['message' => 'success']);
+        } catch (\Exception $e) {
+            throw new GeneralException('', $e->getCode(), $e);
+        }
+    }
+
+    public function show(int $id)
+    {
+        try {
+            $student = Student::findOrFail($id)->first();
+            if (!$student) throw new GeneralException('Student not found', 404);
+            return new StudentResource($student);
+        } catch (\Exception $e) {
+            throw new GeneralException('', $e->getCode(), $e);
+        }
+    }
+
+    public function update(array $data, int $id)
+    {
+        try {
+            $student = Student::findOrFail($id)->first();
+            if (!$student) throw new GeneralException('Student not found', 404);
+            $student->update($data);
+            return new GeneralResource(['message' => 'success']);
+        } catch (\Exception $e) {
+            throw new GeneralException('', $e->getCode(), $e);
+        }
+    }
+
+    public function destroy(int $id)
+    {
+        try {
+            $student = Student::findOrFail($id)->first();
+            if (!$student) throw new GeneralException('Student not found', 404);
+            $student->delete();
+            return new GeneralResource(['message' => 'success']);
+        } catch (\Exception $e) {
+            throw new GeneralException('', $e->getCode(), $e);
+        }
+    }
+}
