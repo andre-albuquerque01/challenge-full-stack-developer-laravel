07c668e35ab82fae0fcceb6b755134a708efdf7d
andre <andrealbuquerque2001@gmail.com>
2024-07-22 13:25:21 -0300

    feat: create action

---
+++ front/src/actions/student/getAll.ts
@@ -0,0 +1,30 @@
+'use server'
+
+import ApiAction from '@/functions/data/apiAction'
+import apiError from '@/functions/error/apiErro'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+export default async function getAllStudent() {
+  try {
+    const response = await ApiAction('/student', {
+      headers: {
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + cookies().get('token')?.value,
+      },
+      next: {
+        revalidate: 30 * 60,
+        tags: ['student'],
+      },
+    })
+    const data = await response.json()
+    if (data.message === 'Unauthenticated.' || !data) {
+      cookies().delete('token')
+      redirect('/')
+    }
+
+    return data.data
+  } catch (error) {
+    return apiError(error)
+  }
+}
