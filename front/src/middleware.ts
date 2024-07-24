285b5198c3980a7da69566fd06865b028cd80274
andre <andrealbuquerque2001@gmail.com>
2024-07-24 11:59:21 -0300

    feat: update middleware

--- front/src/middleware.ts
+++ front/src/middleware.ts
@@ -5,17 +5,20 @@ export async function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value
   const authentication = token ? await verifyToken(token) : false
   if (
-    (!authentication && request.nextUrl.pathname.startsWith('/dashboard')) ||
-    request.nextUrl.pathname.startsWith('/user/update') ||
-    request.nextUrl.pathname.startsWith('student/insert')
+    !authentication &&
+    (request.nextUrl.pathname.startsWith('/dashboard') ||
+      request.nextUrl.pathname.startsWith('/user/update') ||
+      request.nextUrl.pathname.startsWith('/student/insert') ||
+      request.nextUrl.pathname.startsWith('/student/update') ||
+      request.nextUrl.pathname.startsWith('/student/search'))
   ) {
     return NextResponse.redirect(new URL('/', request.url))
   }
 
   if (
     authentication &&
-    request.nextUrl.pathname.endsWith('/') &&
-    request.nextUrl.pathname.startsWith('user/login')
+    (request.nextUrl.pathname.endsWith('/') ||
+      request.nextUrl.pathname.startsWith('user/login'))
   ) {
     return NextResponse.redirect(new URL('/dashboard', request.url))
   }
