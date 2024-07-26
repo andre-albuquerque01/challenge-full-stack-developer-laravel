fb7aea7895085a68cfa999227b7b3c07573907c2
andre <andrealbuquerque2001@gmail.com>
2024-07-26 12:36:22 -0300

    feat new component

---
+++ front/src/components/header/nav.tsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+  return (
+    <header className="">
+      <div className="max-w-[1200px] mx-auto px-5 flex justify-between items-center h-20">
+        <h1 className={`text-2xl`}>
+          grupo <span className="text-7xl">Techcraft</span>
+        </h1>
+      </div>
+    </header>
+  )
+}
