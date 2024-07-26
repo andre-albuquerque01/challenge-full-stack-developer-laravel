2f070f196e9ff68d76e15fb1409781b91fe94bb0
andre <andrealbuquerque2001@gmail.com>
2024-07-26 12:36:14 -0300

    feat new component

---
+++ front/src/components/form/inputPatternComponent.tsx
@@ -0,0 +1,31 @@
+interface InputProps {
+  type: string
+  label: string
+  id: string
+  name: string
+  required: boolean
+  placeHolder?: string
+  value?: string
+}
+
+export const InputPatterComponent = ({ ...props }: InputProps) => {
+  return (
+    <div className="flex items-center h-10 border border-zinc-300 rounded-r-md">
+      <label
+        htmlFor={props.id}
+        className="border h-full w-[20%] flex items-center bg-zinc-200 px-4 font-semibold"
+      >
+        {props.label}{' '}
+        {props.required && <span className="text-xs text-red-600"> *</span>}
+      </label>
+      <input
+        type={props.type}
+        name={props.name}
+        id={props.id}
+        placeholder={props.placeHolder}
+        className="flex-1 p-4 bg-transparent text-sm h-10 outline-none placeholder:text-zinc-500 focus-within:outline-none"
+        required={props.required}
+      />
+    </div>
+  )
+}
